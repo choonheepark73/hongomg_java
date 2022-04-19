ec9e7cde6a0a5bf38925afa4ce2778a716112281
choonheepark73 <74396106+choonheepark73@users.noreply.github.com>
2022-04-19 15:53:40 +0900

    Add files via upload

---
+++ operator/OperatorTest02.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorTest02 {
+
+	public static void main(String[] args) {
+		// 조건연산자(삼항연산자)  조건? :
+		int score = 99;
+		char grade = (score > 90) ? 'A' : 'B';
+		System.out.println(grade);
+
+	}
+
+}
